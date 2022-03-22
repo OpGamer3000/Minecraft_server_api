5f798cf2735c7643cd77aeb56709224ef6a99404
OPGAMER3000PRO <88536432+OpGamer3000@users.noreply.github.com>
2022-03-22 01:05:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from threading import Thread
-from replit import db #the db which i will use
+from replit import db  #the db which i will use
 import os
 from dotenv import load_dotenv
 import time
@@ -16,52 +16,52 @@ api = Flask('')
 
 @api.route('/')
 def home():
-	return "yes"
+    return "yes"
 
 
 @api.route("/stat")
 def stat():
-	stat = db["stat"]
-	return stat
+    stat = db["stat"]
+    return stat
+
 
 @api.route("/setStat")
 def setStat():
-	request.args["pass"]
-	stat = request.args.get("stat")
-	password = request.args.get("pass")
+    request.args["pass"]
+    stat = request.args.get("stat")
+    password = request.args.get("pass")
 
-	if password == PASS:
-		if stat == "true":
-			db["stat"] = "true"
-			return "OK"
-		elif stat == "false":
-			global current_time
-			current_time = time.time()
-			db["stat"] = "false"
-			return "OK"
-		else:
-			return "ERROR"
-	else:
-		return "wrong password"
+    if password == PASS:
+        if stat == "true":
+            db["stat"] = "true"
+            return "OK"
+        elif stat == "false":
+            global current_time
+            current_time = time.time()
+            db["stat"] = "false"
+            return "OK"
+        else:
+            return "ERROR"
+    else:
+        return "wrong password"
+
 
 @api.route("/getDowntime")
 def getDowntime():
-	stat = db["stat"]
-	if stat == "false":
-		global current_time
-		now = time.time()
-		total = int(now - current_time)
-		return str(total)
-	elif stat == "true":
-		return "true"
-	else:
-		return "ERROR"
-		
-	
+    stat = db["stat"]
+    if stat == "false":
+        global current_time
+        now = time.time()
+        total = int(now - current_time)
+        return str(total)
+    elif stat == "true":
+        return "true"
+    else:
+        return "ERROR"
 
 
 def run():
-	api.run(host="0.0.0.0", port=6969)
+    api.run(host="0.0.0.0", port=6969)
 
 
 t = Thread(target=run)
